16071b4da971c1a7f6bd26f9d6a9f5b89af3a2e5
USER <USER@USER-PC>
2017-05-16 10:44:30 +0800

    加入專案檔案。

---
+++ MVCLab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLab.Startup))]
+namespace MVCLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
