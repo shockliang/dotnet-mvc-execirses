b0998060c25ecd1e3bdd33786a1de2c5245e3da2
Shock <shockwater@gmail.com>
2017-05-17 18:18:23 +0800

    Add HelloWorld view

--- MVCLab/Controllers/HelloWorldController.cs
+++ MVCLab/Controllers/HelloWorldController.cs
@@ -12,9 +12,9 @@ namespace MVCLab.Controllers
         // 
         // GET: /HelloWorld/ 
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         // 
